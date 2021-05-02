4dc519b8ac65def9266ef1e8621bbfa14317a64e
Jhonathan Campos <jhonathanaugusto@gmail.com>
2021-05-01 21:29:54 -0300

    creation of the LEARN page and views

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,13 @@ from . import main
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/learn.html')
+def learn():
+    return render_template('learn.html')
+
+
+@main.route('/discover.html')
+def discover():
+    return render_template('discover.html')
